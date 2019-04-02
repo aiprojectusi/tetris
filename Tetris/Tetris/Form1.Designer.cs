34bc66227099c53286e40ed7701666198fb19d20
endoffile78 <endoffile78@users.noreply.github.com>
2019-04-02 17:27:59 -0500

    Work on UI and add tetris blocks

--- Tetris/Tetris/Form1.Designer.cs
+++ Tetris/Tetris/Form1.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.blocksDataGrid = new System.Windows.Forms.DataGridView();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.nextGridView = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.blocksDataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // blocksDataGrid
+            // 
+            this.blocksDataGrid.AllowUserToAddRows = false;
+            this.blocksDataGrid.AllowUserToDeleteRows = false;
+            this.blocksDataGrid.BackgroundColor = System.Drawing.SystemColors.Desktop;
+            this.blocksDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.blocksDataGrid.GridColor = System.Drawing.SystemColors.WindowText;
+            this.blocksDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.blocksDataGrid.Name = "blocksDataGrid";
+            this.blocksDataGrid.ReadOnly = true;
+            this.blocksDataGrid.Size = new System.Drawing.Size(210, 470);
+            this.blocksDataGrid.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(228, 142);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(115, 31);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // nextGridView
+            // 
+            this.nextGridView.AllowUserToAddRows = false;
+            this.nextGridView.AllowUserToDeleteRows = false;
+            this.nextGridView.BackgroundColor = System.Drawing.SystemColors.Desktop;
+            this.nextGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.nextGridView.GridColor = System.Drawing.SystemColors.Desktop;
+            this.nextGridView.Location = new System.Drawing.Point(234, 12);
+            this.nextGridView.Name = "nextGridView";
+            this.nextGridView.ReadOnly = true;
+            this.nextGridView.Size = new System.Drawing.Size(123, 103);
+            this.nextGridView.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(254, 398);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 413);
+            this.ClientSize = new System.Drawing.Size(369, 497);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.nextGridView);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.blocksDataGrid);
             this.Name = "Form1";
             this.Text = "Tetris";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.blocksDataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nextGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView blocksDataGrid;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.DataGridView nextGridView;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- Tetris/Tetris/Form1.cs
+++ Tetris/Tetris/Form1.cs
@@ -16,5 +16,61 @@ namespace Tetris
         {
             InitializeComponent();
         }
+
+        private void setupDataGridView(DataGridView grid)
+        {
+            grid.BackgroundColor = Color.Black;
+            grid.ShowEditingIcon = false;
+            grid.ShowCellToolTips = false;
+            grid.RowHeadersVisible = false;
+            grid.ColumnHeadersVisible = false;
+            grid.AllowUserToResizeRows = false;
+            grid.AllowUserToResizeColumns = false;
+            grid.ShowCellToolTips = false;
+        }
+
+        private void addColumns(DataGridView grid, int columns)
+        {
+            grid.ColumnCount = columns;
+            for (int i = 0; i < columns; i++)
+            {
+                grid.Columns[i].Width = 20;
+            }
+        }
+
+        private void addRows(DataGridView grid, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                grid.Rows.Add();
+                grid.Rows[i].Height = 20;
+                for (int j = 0; j < grid.Rows[i].Cells.Count; j++)
+                {
+                    grid.Rows[i].Cells[j].Style.BackColor = Color.Black;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setupDataGridView(blocksDataGrid);
+            addColumns(blocksDataGrid, 10);
+            addRows(blocksDataGrid, 23);
+
+            setupDataGridView(nextGridView);
+            addColumns(nextGridView, 5);
+            addRows(nextGridView, 5);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            blocksDataGrid.CurrentCell = null;
+            nextGridView.CurrentCell = null;
+        }
     }
 }
